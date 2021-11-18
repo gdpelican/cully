f62e3714c41bfdb04031e65d86474571683988f2
James <james.kiesel@gmail.com>
2021-11-18 18:06:29 +1300

    Fix react className

--- pages/gallery/components/controls.tsx
+++ pages/gallery/components/controls.tsx
@@ -5,7 +5,7 @@ const Controls = () => {
   const { currentPhoto, prev, next, hasPrev, hasNext } = useContext(Context)
 
   return (
-    <div class="cully__controls">
+    <div className="cully__controls">
       <button disabled={!hasPrev} onClick={prev}>BACK</button>
       <span>{currentPhoto.filename}</span>
       <button disabled={!hasNext} onClick={next}>NEXT</button>
